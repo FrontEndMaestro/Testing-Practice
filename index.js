b2d7188564e743245baa6f7f8df3494e2360ff33
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-02-09 23:39:21 +0500

    Add calculator method and its test

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ function capitalize(string) {
   return string.at(0).toUpperCase() + string.slice(1);
 }
 
-function reverse(string){
-    return string.split("").reverse().join("")
-
+function reverse(string) {
+  return string.split("").reverse().join("");
 }
 
-export { capitalize,reverse };
+let calculator = {
+  add: function (num1, num2) {
+    return num1 + num2;
+  },
+  divide: function (num1, num2) {
+    if (num1 != 0 && num2 == 0) return undefined;
+    return num1 / num2;
+  },
+  multiply:function(n1,n2){
+    return n1*n2
+  }
+};
+
+export { capitalize, reverse, calculator };
--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,5 @@
 //A capitalize function that takes a string and returns it with the first character capitalized.
-import { capitalize,reverse } from "./index.js";
+import { capitalize, reverse, calculator } from "./index.js";
 
 describe("Capitalize", () => {
   it("First character should be capitalized", () => {
@@ -14,15 +14,58 @@ describe("Capitalize", () => {
   });
 });
 
-describe("Reverse string",()=>{
+describe("Reverse string", () => {
+  it("String should be reversed", () => {
+    let tests = [
+      { input: "hello", output: "olleh" },
+      { input: "kanye", output: "eynak" },
+    ];
+    tests.forEach((t) => {
+      expect(reverse(t.input)).toBe(t.output);
+    });
+  });
+});
 
-it("String should be reversed",()=>{
+describe("Calculator", () => {
+  it("should exist", () => {
+    expect(calculator).toBeDefined();
+  });
+  it("add method should exist", () => {
+    expect(calculator.add).toBeDefined();
+  });
 
-    expect(reverse("hello")).toBe('olleh')
-    expect(reverse("kanye")).toBe("eynak")
+  it("add two numbers", () => {
+    expect(calculator.add(2, 2)).toBe(4);
+    expect(calculator.add(4, 6)).toBe(10);
+  });
 
-})
+  it("divide should exist", () => {
+    expect(calculator.divide).toBeDefined();
+  });
+  it("divide should be a method", () => {
+    expect(typeof calculator.divide).toBe("function");
+  });
 
-})
+  it("divide two numbers", () => {
+    let tests = [
+      { n1: 10, n2: 2, output: 5 },
+      { n1: 8, n2: 5, output: 1.6 },
+      { n1: 100, n2: 0, output: undefined },
+      { n1: 0, n2: 100, output: 0 },
+    ];
+    tests.forEach((t) => {
+      expect(calculator.divide(t.n1, t.n2)).toBe(t.output);
+    });
+  });
 
-
+  it("multiply should exist", () => {
+    expect(calculator.multiply).toBeDefined();
+  });
+  it("multiply should be a method", () => {
+    expect(typeof calculator.multiply).toBe("function");
+  });
+  it("multiply two numbers",()=>{
+    expect(calculator.multiply(2,4)).toBe(8)
+    expect(calculator.multiply(5,1.78)).toBe(8.9)
+  })
+});
